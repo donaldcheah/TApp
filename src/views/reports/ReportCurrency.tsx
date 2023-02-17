0884d8ab8ad5a6ea5f8a4c7744b8d6de9e21e50f
Donald Lovelace <donaldcheahch@gmail.com>
2023-02-17 19:53:45 +0800

    add another currency, USDC, to the list of assets to calculate against MYR. added list of target currencies to title of view.

--- src/views/reports/ReportCurrency.tsx
+++ src/views/reports/ReportCurrency.tsx
@@ -16,35 +16,52 @@ transaction object
     payment
 }
 */
+const TargetCurrencies = ['USDT', 'USDC']
+const BaseCurrency = 'MYR'
+
 const viewStyle: CSSProperties = {
     padding: '8px',
     border: '1px solid',
     borderRadius: '8px',
     marginBottom: '8px'
 }
+const currencyStyle: CSSProperties = {
+    border: '1px solid',
+    borderRadius: '8px',
+    marginBottom: '8px',
+    padding: '8px',
+}
+const currencyTitleStyle: CSSProperties = {
+    margin: '0px'
+}
 class ReportCurrency extends React.Component<Props> {
     renderReport() {
-        let myrAmount: Decimal = new Decimal(0);
-        let usdtAmount: Decimal = new Decimal(0);
-        this.props.transactions.filter((t) => {
-            const { from, to } = t;
-            return from.toUpperCase() === 'MYR' && to.toUpperCase() === 'USDT'
-        }).forEach((t) => {
-            const { fromAmount, toAmount } = t;
-            myrAmount = myrAmount.add(fromAmount)
-            usdtAmount = usdtAmount.add(toAmount)
+        return TargetCurrencies.map((c) => {
+            let myrAmount: Decimal = new Decimal(0);
+            let usdtAmount: Decimal = new Decimal(0);
+            this.props.transactions.filter((t) => {
+                const { from, to } = t;
+                return from.toUpperCase() === BaseCurrency && to.toUpperCase() === c
+            }).forEach((t) => {
+                const { fromAmount, toAmount } = t;
+                myrAmount = myrAmount.add(fromAmount)
+                usdtAmount = usdtAmount.add(toAmount)
+            })
+            if (myrAmount.toNumber() === 0 || usdtAmount.toNumber() === 0) {
+                return <p key={c}>no data to display</p>
+            }
+            return <div key={c} style={currencyStyle}>
+                <h5 style={currencyTitleStyle}>{c}</h5>
+                <hr />
+                <p>Sold {myrAmount.toFixed(2)} {BaseCurrency} for {usdtAmount.toNumber()} {c}</p>
+                <p>Ratio of {(myrAmount.dividedBy(usdtAmount)).toFixed(2)} {BaseCurrency}/{c}</p>
+            </div>
         })
-        if (myrAmount.toNumber() === 0 || usdtAmount.toNumber() === 0) {
-            return <p>no data to display</p>
-        }
-        return <div>
-            <p>Sold {myrAmount.toFixed(2)} MYR for {usdtAmount.toNumber()} USDT</p>
-            <p>Ratio of {(myrAmount.dividedBy(usdtAmount)).toFixed(2)} MYR/USDT</p>
-        </div>
     }
+
     render(): React.ReactNode {
         return <div id="reportCurrency" style={viewStyle}>
-            <h5>Currency</h5>
+            <h5>Currency ({TargetCurrencies.join(',')})</h5>
             {this.renderReport()}
         </div>
     }
